d053e8c4cf5e16fd47eb7036061348087b5e1e4c
MAURO GAY <MAURO.GAY@student.intecbrussel.be>
2020-11-10 09:53:46 +0100

    save

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,11 +6,38 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    enum WorkingDays
+    { 
+    Monday,  //0                                                                            //Al deze dagen zijn Integers (geen strings)
+    Thuesday,//1
+    Wednesday=3,      //2   //je kan adres zelf kiezen (vb om 0 weg te laten en vanaf 1 beginnen tellen)(
+    Thurseday,//3           // word 4
+    Friday//4               // word 5
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            string day = "Monday";
+            //WorkingDays d = (WorkingDays)1;
+            //Console.WriteLine(d);
+
+            //WorkingDays d = WorkingDays.Wednesday;
+            //Console.WriteLine(d);
+
+
+            //WorkingDays a = WorkingDays.Friday;
+            //Console.WriteLine((int)a);
+
+
+            foreach (int i in Enum.GetValues(typeof(WorkingDays)))              //Enum.GetValues(typof     Toont alle plaatsen 0,1,2,3,4(int)     ENUMARATE VALUES
+            {
+                Console.WriteLine(i);
+            }
+
+            foreach (string j in Enum.GetNames(typeof(WorkingDays)))            //Enum.GetNames(typeof     Toont alle dagen(strings)                ENUMARATE NAMES
+            {
+                Console.WriteLine(j);
+            }
         }
     }
 }
